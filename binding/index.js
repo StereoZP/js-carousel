b411c1e35e039c805d6323c1f6ec285e90716ff3
ElectricalVeins <maxmost512@gmail.com>
2022-10-07 20:27:43 +0300

    Fix bug

--- binding/index.js
+++ binding/index.js
@@ -3,17 +3,17 @@ import carouselController from "./createModel.js";
 
 const {controls, dotsContainer} = view;
 const [initializeCarousel, carouselLogic] = carouselController;
-const [next, prev] = controls;
+const [prev, next] = controls;
 
 /* Create "View-Model" - Connect rendered View with Model logic*/
 [...dotsContainer.children].forEach((dot, index) => {
     dot.onclick = () => carouselLogic.setSlide(index + 1)
 })
 prev.addEventListener("click", () => {
-    carouselLogic.setSlide(carouselLogic.getNextSlide());
+    carouselLogic.setSlide(carouselLogic.getPrevSlide());
 });
 next.addEventListener("click", () => {
-    carouselLogic.setSlide(carouselLogic.getPrevSlide());
+    carouselLogic.setSlide(carouselLogic.getNextSlide());
 });
 
 export default function createBinding() {
